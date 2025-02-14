5c5e88674a2c918fc9cdc6b81be6f68d6863f293
jingyang022 <jingyang022@yahoo.com.sg>
2025-02-14 13:05:18 +0800

    added files

--- ecs-cluster.tf
+++ ecs-cluster.tf
@@ -1,36 +1,19 @@
-resource "aws_ecs_cluster" "ecs_cluster" {
- name = "yap-flask-ecs-cluster"
+resource "aws_ecr_repository" "ecr" {
+  name         = "yap-ecr"
+  force_delete = true
 }
 
-# resource "aws_autoscaling_group" "ecs_asg" {
-#  vpc_zone_identifier = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
-#  desired_capacity    = 2
-#  max_size            = 3
-#  min_size            = 1
+module "ecs" {
+  source  = "terraform-aws-modules/ecs/aws"
+  version = "~> 5.9.0"
 
-#  launch_template {
-#    id      = aws_launch_template.ecs_lt.id
-#    version = "$Latest"
-#  }
-
-#  tag {
-#    key                 = "AmazonECSManaged"
-#    value               = true
-#    propagate_at_launch = true
-#  }
-# }
-
-resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
- name = "test1"
-
- auto_scaling_group_provider {
-   auto_scaling_group_arn = aws_autoscaling_group.ecs_asg.arn
-
-   managed_scaling {
-     maximum_scaling_step_size = 1000
-     minimum_scaling_step_size = 1
-     status                    = "ENABLED"
-     target_capacity           = 3
-   }
- }
+  cluster_name = "yap-ecs"
+  
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 100
+      }
+    }
+  }
 }
